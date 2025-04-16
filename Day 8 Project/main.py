8b90175f83e6dae1b68739eab0e3762755933e01
ryanwallace <ryan.a.wallace64@gmail.com>
2025-04-16 16:48:20 -0500

    Add files via upload

---
+++ Day 8 Project/main.py
@@ -0,0 +1,36 @@
+from art import logo
+
+alphabet = ['aenc', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+game_over = False
+
+
+def caesar(original_text, shift_amount, encode_or_decode):
+    output_text = ""
+
+    if encode_or_decode == "decode":
+        shift_amount *= -1
+
+    for letter in original_text:
+        if letter not in alphabet:
+            output_text += letter
+        else:
+            shifted_position = alphabet.index(letter) + shift_amount
+            shifted_position %= len(alphabet)
+            output_text += alphabet[shifted_position]
+    print(f"Here is the result: {output_text}")
+
+
+while not game_over:
+    print(logo)
+
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    caesar(original_text=text, shift_amount=shift, encode_or_decode=direction)
+
+    keep_playing = input("Would you like to use the cypher again? 'y' or 'n'? ")
+    if keep_playing == 'n':
+        game_over = True
+
+print("\nThank you for using Caesar's Cipher!")
